7aef375fd2583475a2d34584acb77308cbe0d156
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2026-01-18 00:38:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from scripts.generar_dataset_espermatozoides import main as generar_dataset_espermatozoides
-
+from scripts.generar_dataset_rps import generar_datos as generar_dataset_rps
 
 def main():
+    print("\n--- INICIANDO PIPELINE: ESPERMATOZOIDES ---")
     generar_dataset_espermatozoides()
 
+    print("--- INICIANDO PIPELINE: PIEDRA, PAPEL O TIJERA ---")
+    generar_dataset_rps()
+
+    print("\n--- TODOS LOS PROCESOS FINALIZADOS ---")
 
 if __name__ == "__main__":
     main()
