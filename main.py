2e9a9e5880bac7ca393184a22b62f15dda5253f4
Pablo Paucar <pablopaucar2004@gmail.com>
2026-01-17 23:46:37 -0500

    Estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,9 @@
+from scripts.generar_dataset_espermatozoides import main as generar_dataset_espermatozoides
+
+
+def main():
+    generar_dataset_espermatozoides()
+
+
+if __name__ == "__main__":
+    main()
