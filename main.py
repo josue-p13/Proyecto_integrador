d050693b6f5a8264ccc96a0b2f802de0dae964f7
Josue Pinza <jhosuepin@hotmail.com>
2026-01-20 00:02:04 -0500

    Añadir en el main la funcion que esta en script y consume todas

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from scripts.generar_dataset_espermatozoides import generar_datos as generar_dataset_espermatozoides
 from scripts.generar_dataset_rps import generar_datos as generar_dataset_rps
+from scripts.extraer_caracteristicas import extraer_todas_caracteristicas
 
 def main():
     print("\n--- INICIANDO PIPELINE: ESPERMATOZOIDES ---")
     generar_dataset_espermatozoides()
 
-    print("--- INICIANDO PIPELINE: PIEDRA, PAPEL O TIJERA ---")
+    print("\n--- INICIANDO PIPELINE: PIEDRA, PAPEL O TIJERA ---")
     generar_dataset_rps()
+    
+    print("\n--- EXTRAYENDO TODAS LAS CARACTERISTICAS ---")
+    extraer_todas_caracteristicas()
 
     print("\n--- TODOS LOS PROCESOS FINALIZADOS ---")
 
