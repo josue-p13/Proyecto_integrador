d5176c18622a201c8e3771a8b151226c593c0ee8
CrisAti <daniati2004@hotmail.com>
2026-01-23 21:59:29 -0500

    feat: generación de embeddings para espermatozoides

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from scripts.generar_dataset_espermatozoides import generar_datos as generar_dataset_espermatozoides
 from scripts.generar_dataset_rps import generar_datos as generar_dataset_rps
 from scripts.extraer_caracteristicas import extraer_todas_caracteristicas
-
+from scripts.generar_embeddings_espermatozoides import generar_embeddings_espermatozoides
+from scripts.generar_embeddings_rps import generar_embeddings_rps
 def main():
     print("\n--- INICIANDO PIPELINE: ESPERMATOZOIDES ---")
     generar_dataset_espermatozoides()
@@ -12,6 +13,9 @@ def main():
     print("\n--- EXTRAYENDO TODAS LAS CARACTERISTICAS ---")
     extraer_todas_caracteristicas()
 
+    print("\n--- EXTRAYENDO LOS EMBEDDINGS ---")
+    generar_embeddings_espermatozoides()
+    generar_embeddings_rps()
     print("\n--- TODOS LOS PROCESOS FINALIZADOS ---")
 
 if __name__ == "__main__":
